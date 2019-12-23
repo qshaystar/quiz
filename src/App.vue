041b4430f640f4a5601728c28812223e5b837264
Sasha Ye <akomat1103@gmail.com>
2019-12-23 17:59:36 +0800

    fundamental ui ok

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header></Header>
+    <b-row>  
+      <b-col sm="6" offset="3">
+        <QuestionBox></QuestionBox>
+      </b-col>
+    </b-row>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+import Header from './components/Header.vue'
+import QuestionBox from './components/QuestionBox.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    QuestionBox
   }
 }
 </script>
